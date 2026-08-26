4b9e7dd5fc625662706bec9428f5acade6f2c7d1
Harness Auto-Remediation <remediation@harness.io>
2026-08-26 06:43:55 +0000

    vuln-fix: Use static require path for config loader (STO-W-xoqRRQ66UHp8H2I9rwfX)
    Weakness: 95
    Severity: Medium
    Detection: Tool
    Signed-off-by: Harness Auto-Remediation <remediation@harness.io>
    Co-authored-by: Harness Auto-Remediation <remediation@harness.io>

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ const util = require("util");
 
 const finalEnv = process.env.NODE_ENV || "development";
 
-const allConf = require(path.resolve(__dirname + "/../config/env/all.js"));
+const allConf = require("./env/all.js");
 const envConf = require(path.resolve(__dirname + "/../config/env/" + finalEnv.toLowerCase() + ".js")) || {};
 
 const config = { ...allConf, ...envConf };
