73b36823e791abcfc2a3a8ce526a32e6b35aaa45
Harness Auto-Remediation <remediation@harness.io>
2026-08-26 06:36:27 +0000

    vuln-fix: upgrade http references to https for transport sec (STO-1OU7UuS2grNvYgxDacKxUI)
    Weakness: 419
    Severity: Low
    Detection: Tool
    Signed-off-by: Harness Auto-Remediation <remediation@harness.io>
    Co-authored-by: Harness Auto-Remediation <remediation@harness.io>

--- server.js
+++ server.js
@@ -9,13 +9,6 @@ const consolidate = require("consolidate"); // Templating library adapter for Ex
 const swig = require("swig");
 // const helmet = require("helmet");
 const MongoClient = require("mongodb").MongoClient; // Driver for connecting to MongoDB
-const http = require("http");
-const marked = require("marked");
-//const nosniff = require('dont-sniff-mimetype');
-const app = express(); // Web framework to handle routing requests
-const routes = require("./app/routes");
-const { port, db, cookieSecret } = require("./config/config"); // Application config properties
-/*
 // Fix for A6-Sensitive Data Exposure
 // Load keys for establishing secure HTTPS connection
 const fs = require("fs");
@@ -25,8 +18,11 @@ const httpsOptions = {
     key: fs.readFileSync(path.resolve(__dirname, "./artifacts/cert/server.key")),
     cert: fs.readFileSync(path.resolve(__dirname, "./artifacts/cert/server.crt"))
 };
-*/
-
+const marked = require("marked");
+//const nosniff = require('dont-sniff-mimetype');
+const app = express(); // Web framework to handle routing requests
+const routes = require("./app/routes");
+const { port, db, cookieSecret } = require("./config/config"); // Application config properties
 MongoClient.connect(db, (err, db) => {
     if (err) {
         console.log("Error: DB: connect");
@@ -137,17 +133,10 @@ MongoClient.connect(db, (err, db) => {
         autoescape: true
     });
 
-    // Insecure HTTP connection
-    http.createServer(app).listen(port, () => {
-        console.log(`Express http server listening on port ${port}`);
-    });
-
-    /*
     // Fix for A6-Sensitive Data Exposure
     // Use secure HTTPS protocol
     https.createServer(httpsOptions, app).listen(port, () => {
-        console.log(`Express http server listening on port ${port}`);
+        console.log(`Express https server listening on port ${port}`);
     });
-    */
 
 });
